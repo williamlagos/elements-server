b8a896c268d764abcbebba3ca37f46d7c4577d3d
William Oliveira de Lagos <william.lagos@icloud.com>
2023-10-24 17:32:48 +0200

    feat: simplified and updated web handler module

--- app.py
+++ app.py
@@ -1,36 +1,17 @@
-#!/usr/bin/env python
-#
-# This file is part of elements project.
-# 
-# Copyright (C) 2009-2011 William Oliveira de Lagos <william.lagos@icloud.com>
-#
-# Elements is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Elements is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with elements.  If not, see <http://www.gnu.org/licenses/>.
-#
+'''Web handler module'''
 
-# from elements.engine.ranking import Nodes
-from elements.server.http import Runtime,Coronae
-import sys,json
+from fastapi import FastAPI
 
-class NodeSample(Coronae):
-	def get(self):
-		# nodes = Nodes()
-		# nodes.prepare_elements('objects.json')
-		# self.write('<pre>'+self.to_json(nodes.elements)+' %i</pre>'%nodes.tree.depth)
-		self.write('<pre>%s</pre>'%json.load(open('elements.json')))
+app = FastAPI()
 
-def main(argv):
-	runtime = Runtime([('/',NodeSample)])
-	runtime.run()
 
-if __name__ == "__main__": main(sys.argv)
+@app.get("/")
+def read_root():
+    '''Root function'''
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    '''Read item function'''
+    return {"item_id": item_id, "q": q}
